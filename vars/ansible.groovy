4baacb5c6e6ca2f175239a91d88fa9c265006a2f
pranshu-bali97 <105100319+pranshu-bali97@users.noreply.github.com>
2024-11-29 16:51:30 +0530

    Update ansible.groovy

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -1,9 +1,3 @@
 def call() {
-    ansiblePlaybook(
-        credentialsId: config.credentialsId ?: '67566dc8-069e-4c59-8308-aac784619a7b',
-        installation: config.installation ?: 'Ansible',
-        inventory: config.inventory ?: 'workspace/resources/aws_ec2.yml',
-        playbook: config.playbook ?: 'workspace/resources/playbook.yml',
-        vaultTmpPath: config.vaultTmpPath ?: ''
-    )
+    ansiblePlaybook credentialsId: '67566dc8-069e-4c59-8308-aac784619a7b', installation: 'Ansible', inventory: 'resources/aws_ec2.yml', playbook: 'resources/playbook.yml', vaultTmpPath: ''
 }
